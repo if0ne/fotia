beef1db9102d1516283d2e76ad25c328b913a4ed
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2025-03-13 16:26:53 +0300

    added(dx): fence

--- src/rhi/dx12/command.rs
+++ src/rhi/dx12/command.rs
@@ -1,5 +1,10 @@
+use std::sync::{Arc, atomic::AtomicU64};
+
+use oxidx::dx::{self, IDevice, IFence};
+
 use crate::rhi::command::{
-    IoCommandBuffer, RenderCommandDevice, RenderCommandQueue, RenderResourceUploader,
+    GpuEvent, IoCommandBuffer, RenderCommandDevice, RenderCommandQueue, RenderResourceUploader,
+    SyncPoint,
 };
 
 use super::{
@@ -10,7 +15,7 @@ use super::{
 impl RenderCommandDevice for DxDevice {
     type ResourceUploader = DxResourceUploader;
     type CommandQueue = DxCommandQueue;
-    type Event = ();
+    type Event = DxFence;
 
     fn create_command_queue(
         &self,
@@ -25,11 +30,41 @@ impl RenderCommandDevice for DxDevice {
     }
 
     fn create_event(&self, shared: bool) -> Self::Event {
-        todo!()
+        let flags = if shared {
+            dx::FenceFlags::Shared | dx::FenceFlags::SharedCrossAdapter
+        } else {
+            dx::FenceFlags::empty()
+        };
+
+        let fence = self
+            .gpu
+            .create_fence(0, flags)
+            .expect("failed to create fence");
+
+        DxFence {
+            fence,
+            value: Default::default(),
+            shared,
+        }
     }
 
     fn open_event(&self, event: &Self::Event, other_gpu: &Self) -> Self::Event {
-        todo!()
+        let handle = other_gpu
+            .gpu
+            .create_shared_handle(&event.fence, None)
+            .expect("Failed to open handle");
+
+        let open_fence: dx::Fence = self
+            .gpu
+            .open_shared_handle(handle)
+            .expect("Failed to open heap");
+        handle.close().expect("Failed to close handle");
+
+        DxFence {
+            fence: open_fence,
+            value: Arc::clone(&event.value),
+            shared: event.shared,
+        }
     }
 
     fn wait_idle(&self) {
@@ -40,7 +75,7 @@ impl RenderCommandDevice for DxDevice {
 pub struct DxCommandQueue {}
 
 impl RenderCommandQueue for DxCommandQueue {
-    type Event = ();
+    type Event = DxFence;
     type CommandBuffer = ();
 
     fn create_command_buffer(&self) -> Self::CommandBuffer {
@@ -134,5 +169,44 @@ impl IoCommandBuffer<DxDevice> for DxIoCommandBuffer {
 
     fn load_to_texture(&self, texture: &DxTexture, data: &'_ [u8]) {
         todo!()
+    }
+}
+
+#[derive(Debug)]
+pub struct DxFence {
+    pub(super) fence: dx::Fence,
+    pub(super) value: Arc<AtomicU64>,
+    pub(super) shared: bool,
+}
+
+impl GpuEvent for DxFence {
+    fn wait(&self, value: SyncPoint) -> bool {
+        if self.get_completed_value() < value {
+            let event = dx::Event::create(false, false).expect("failed to create event");
+            self.fence
+                .set_event_on_completion(value, event)
+                .expect("failed to bind fence to event");
+            if event.wait(10_000_000) == 0x00000102 {
+                panic!("device lost")
+            }
+
+            true
+        } else {
+            false
+        }
+    }
+
+    fn increment(&self) -> SyncPoint {
+        self.value
+            .fetch_add(1, std::sync::atomic::Ordering::Relaxed)
+            + 1
+    }
+
+    fn get_completed_value(&self) -> SyncPoint {
+        self.fence.get_completed_value()
+    }
+
+    fn get_goal(&self) -> SyncPoint {
+        self.value.load(std::sync::atomic::Ordering::Relaxed)
     }
 }
