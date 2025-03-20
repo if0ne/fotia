8a229a86a81a2f0a835b61fca4441790db614f95
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2025-03-20 21:33:05 +0300

    added: adapted shaders from multi-gpu-shadows for RA

--- assets/shaders/Zpass.hlsl
+++ assets/shaders/Zpass.hlsl
@@ -1,4 +1,11 @@
-cbuffer ObjectTransform : register(b0)
+#include "Common.hlsl"
+#include "Pbr.hlsl"
+
+cbuffer GlobalBuffer : register(b0, space0) {
+    Globals g_data;
+}
+
+cbuffer ObjectTransform : register(b1, space1)
 {
     matrix transform;
 }
@@ -15,7 +22,7 @@ PixelInput Main(VertexInput input) {
     PixelInput output = (PixelInput) 0;
 
     float4 world_pos = mul(transform, float4(input.pos, 1.0f));
-    output.pos = world_pos;
+    output.pos = mul(g_data.proj_view, world_pos);
 
     return output;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::cell::RefCell;
+use std::{cell::RefCell, path::PathBuf};
 
 use ra::{
+    backend::Backend,
     command::{Barrier, RenderCommandContext, RenderCommandEncoder, RenderEncoder},
     context::{ContextDual, RenderDevice},
     swapchain::{RenderSwapchainContext, Surface, Swapchain},
@@ -9,8 +10,9 @@ use ra::{
 use rhi::{
     backend::{Api, DebugFlags},
     command::CommandType,
+    shader::{CompiledShader, ShaderDesc},
     swapchain::{PresentMode, SwapchainDesc},
-    types::ResourceState,
+    types::{ResourceState, ShaderType},
 };
 use timer::GameTimer;
 use tracing::info;
@@ -40,6 +42,8 @@ fn main() {
 
     let backend = rs.dx_backend().expect("failed to get directx backend");
 
+    let shaders = ShaderCollection::new(&backend, cfg!(debug_assertions));
+
     let primary = backend.create_device(0);
     let secondary = backend.create_device(1);
 
@@ -55,6 +59,8 @@ fn main() {
         rs,
         context: group,
         wnd_ctx: None,
+
+        shaders,
     };
 
     event_loop.run_app(&mut app).expect("failed to run app");
@@ -74,6 +80,8 @@ pub struct Application<D: RenderDevice> {
     pub wnd_ctx: Option<WindowContext<D>>,
     pub rs: RenderSystem,
     pub context: ContextDual<D>,
+
+    pub shaders: ShaderCollection,
 }
 
 impl<D: RenderDevice> winit::application::ApplicationHandler for Application<D> {
@@ -233,5 +241,87 @@ impl<D: RenderDevice> Application<D> {
                 });
             }
         })
+    }
+}
+
+pub struct ShaderCollection {
+    pub csm: CompiledShader,
+    pub fullscreen: CompiledShader,
+    pub directional_light_pass: CompiledShader,
+    pub gamma_corr_pass: CompiledShader,
+    pub zpass: CompiledShader,
+    pub gpass_vs: CompiledShader,
+    pub gpass_ps: CompiledShader,
+}
+
+impl ShaderCollection {
+    pub fn new<A: Api<Device: RenderDevice>>(api: &Backend<A>, debug: bool) -> Self {
+        let asset_path = PathBuf::from("../assets/shaders");
+
+        let csm = api.compile_shader(&ShaderDesc {
+            ty: ShaderType::Vertex,
+            path: asset_path.join("Csm.hlsl"),
+            entry_point: "Main".into(),
+            debug,
+            defines: vec![],
+        });
+
+        let fullscreen = api.compile_shader(&ShaderDesc {
+            ty: ShaderType::Vertex,
+            path: asset_path.join("FullscreenVS.hlsl"),
+            entry_point: "Main".into(),
+            debug,
+            defines: vec![],
+        });
+
+        let directional_light_pass = api.compile_shader(&ShaderDesc {
+            ty: ShaderType::Pixel,
+            path: asset_path.join("DirectionalLight.hlsl"),
+            entry_point: "Main".into(),
+            debug,
+            defines: vec![],
+        });
+
+        let gamma_corr_pass = api.compile_shader(&ShaderDesc {
+            ty: ShaderType::Pixel,
+            path: asset_path.join("GammaCorr.hlsl"),
+            entry_point: "Main".into(),
+            debug,
+            defines: vec![],
+        });
+
+        let zpass = api.compile_shader(&ShaderDesc {
+            ty: ShaderType::Vertex,
+            path: asset_path.join("Zpass.hlsl"),
+            entry_point: "Main".into(),
+            debug,
+            defines: vec![],
+        });
+
+        let gpass_vs = api.compile_shader(&ShaderDesc {
+            ty: ShaderType::Vertex,
+            path: asset_path.join("GPass.hlsl"),
+            entry_point: "VSMain".into(),
+            debug,
+            defines: vec![],
+        });
+
+        let gpass_ps = api.compile_shader(&ShaderDesc {
+            ty: ShaderType::Pixel,
+            path: asset_path.join("GPass.hlsl"),
+            entry_point: "PSMain".into(),
+            debug,
+            defines: vec![],
+        });
+
+        Self {
+            csm,
+            fullscreen,
+            directional_light_pass,
+            gamma_corr_pass,
+            zpass,
+            gpass_vs,
+            gpass_ps,
+        }
     }
 }
