2bb1ebb7abb8e2069f3d4e66e3702adee7639ea2
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2025-03-22 17:16:35 +0300

    added: camera controller by dolly crate

--- src/engine/camera.rs
+++ src/engine/camera.rs
@@ -1,14 +1,96 @@
-#[derive(Clone, Debug)]
+use std::collections::HashMap;
+
+use dolly::{prelude::*, rig::CameraRig};
+use winit::keyboard::{KeyCode, PhysicalKey};
+
+#[derive(Debug)]
 pub struct Camera {
-    pub view: glam::Mat4,
     pub far: f32,
     pub near: f32,
     pub fov: f32,
     pub aspect_ratio: f32,
+    pub rig: CameraRig<LeftHanded>,
 }
 
 impl Camera {
+    pub fn new(
+        near: f32,
+        far: f32,
+        fov: f32,
+        extent: [u32; 2],
+        rig: CameraRig<LeftHanded>,
+    ) -> Self {
+        Self {
+            far,
+            near,
+            fov,
+            aspect_ratio: extent[0] as f32 / extent[1] as f32,
+            rig,
+        }
+    }
+
     pub fn proj(&self) -> glam::Mat4 {
         glam::Mat4::perspective_lh(self.fov, self.aspect_ratio, self.near, self.far)
+    }
+
+    pub fn view(&self) -> glam::Mat4 {
+        glam::Mat4::from_rotation_translation(
+            self.rig.final_transform.rotation.into(),
+            self.rig.final_transform.position.into(),
+        )
+    }
+
+    pub fn resize(&mut self, extent: [u32; 2]) {
+        self.aspect_ratio = extent[0] as f32 / extent[1] as f32;
+    }
+
+    pub fn update(&mut self, keys: &HashMap<PhysicalKey, bool>, dt: f32) {
+        let mut direction = glam::Vec3::ZERO;
+
+        if keys
+            .get(&PhysicalKey::Code(KeyCode::KeyW))
+            .is_some_and(|v| *v)
+        {
+            direction.z += 1.0;
+        }
+
+        if keys
+            .get(&PhysicalKey::Code(KeyCode::KeyS))
+            .is_some_and(|v| *v)
+        {
+            direction.z -= 1.0;
+        }
+
+        if keys
+            .get(&PhysicalKey::Code(KeyCode::KeyD))
+            .is_some_and(|v| *v)
+        {
+            direction.x += 1.0;
+        }
+
+        if keys
+            .get(&PhysicalKey::Code(KeyCode::KeyA))
+            .is_some_and(|v| *v)
+        {
+            direction.x -= 1.0;
+        }
+
+        direction = direction.normalize();
+
+        if direction.length() > f32::EPSILON {
+            let quat: glam::Quat = self.rig.final_transform.rotation.into();
+
+            self.rig
+                .driver_mut::<dolly::prelude::Position>()
+                .translate(quat * direction * dt * 10.0);
+
+            self.rig.update(dt);
+        }
+    }
+
+    pub fn rotate(&mut self, dx: f32, dy: f32) {
+        self.rig
+            .driver_mut::<YawPitch>()
+            .rotate_yaw_pitch(-0.3 * dx as f32, -0.3 * dy as f32);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::{cell::RefCell, sync::Arc};
+use std::{cell::RefCell, collections::HashMap, sync::Arc};
 
 use collections::handle::Handle;
+use dolly::{prelude::*, rig::CameraRig};
 use engine::camera::Camera;
+use glam::vec2;
 use hecs::World;
 use multi_gpu_renderer::{
     GpuGlobals, graphs::single_gpu::SingleGpuShadows, pso::PsoCollection, shaders::ShaderCollection,
@@ -17,6 +19,7 @@ use ra::{
 use rhi::{
     backend::{Api, DebugFlags},
     command::CommandType,
+    dx12::device::DxDevice,
     resources::BufferUsages,
     swapchain::{PresentMode, SwapchainDesc},
     types::ResourceState,
@@ -24,7 +27,10 @@ use rhi::{
 use timer::GameTimer;
 use tracing::info;
 use tracing_subscriber::layer::SubscriberExt;
-use winit::raw_window_handle::{HasWindowHandle, RawWindowHandle};
+use winit::{
+    keyboard::PhysicalKey,
+    raw_window_handle::{HasWindowHandle, RawWindowHandle},
+};
 
 pub mod collections;
 pub mod engine;
@@ -34,108 +40,6 @@ pub mod timer;
 
 mod multi_gpu_renderer;
 
-fn main() {
-    unsafe {
-        std::env::set_var("RUST_BACKTRACE", "1");
-    }
-    let console_log = tracing_subscriber::fmt::Layer::new()
-        .with_ansi(true)
-        .with_writer(std::io::stdout);
-    let subscriber = tracing_subscriber::registry().with(console_log);
-    let _ = tracing::subscriber::set_global_default(subscriber);
-
-    let rs = Arc::new(RenderSystem::new(&[RenderBackendSettings {
-        api: RenderBackend::Dx12,
-        debug: if cfg!(debug_assertions) {
-            DebugFlags::all()
-        } else {
-            DebugFlags::empty()
-        },
-    }]));
-
-    let backend = rs.dx_backend().expect("failed to get directx backend");
-
-    let shaders = ShaderCollection::new(&backend, cfg!(debug_assertions));
-
-    let primary = backend.create_device(0);
-    let secondary = backend.create_device(1);
-
-    let group = Arc::new(ContextDual::new(primary, secondary));
-
-    let psos = PsoCollection::new(Arc::clone(&rs), Arc::clone(&group), &shaders);
-
-    let single_gpu = SingleGpuShadows::new(
-        Arc::clone(&rs),
-        Arc::clone(&group.primary),
-        [800, 600],
-        &psos,
-        3,
-    );
-
-    let world = World::new();
-    let globals = GpuGlobals::default();
-    let camera = Camera {
-        view: Default::default(),
-        far: 1000.0,
-        near: 0.1,
-        fov: 90.0f32.to_radians(),
-        aspect_ratio: 800.0 / 600.0,
-    };
-
-    let buffer = rs.create_buffer_handle();
-    let global_argument = rs.create_shader_argument_handle();
-
-    group.call(|ctx| {
-        ctx.bind_buffer(
-            buffer,
-            rhi::resources::BufferDesc::cpu_to_gpu(
-                size_of::<GpuGlobals>() * 3,
-                BufferUsages::Uniform,
-            )
-            .with_name("Global data".into()),
-            None,
-        );
-
-        ctx.bind_shader_argument(
-            global_argument,
-            ra::shader::ShaderArgumentDesc {
-                views: &[],
-                samplers: &[],
-                dynamic_buffer: Some(buffer),
-            },
-        );
-    });
-
-    let event_loop = winit::event_loop::EventLoop::new().expect("failed to create event loop");
-
-    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
-
-    let mut app = Application {
-        timer: GameTimer::default(),
-
-        rs,
-        context: group,
-        wnd_ctx: None,
-
-        shaders,
-        psos,
-        single_gpu,
-
-        world,
-        frames_in_flight: 3,
-        frame_idx: 0,
-        camera,
-
-        globals,
-        buffer,
-        global_argument,
-    };
-
-    event_loop.run_app(&mut app).expect("failed to run app");
-
-    app.context.call(|ctx| ctx.wait_idle());
-}
-
 pub struct WindowContext<D: RenderDevice> {
     pub window: winit::window::Window,
     pub wnd: RawWindowHandle,
@@ -143,6 +47,10 @@ pub struct WindowContext<D: RenderDevice> {
 }
 
 pub struct Application<D: RenderDevice> {
+    pub title: String,
+    pub width: u32,
+    pub height: u32,
+
     pub timer: GameTimer,
 
     pub world: World,
@@ -156,19 +64,234 @@ pub struct Application<D: RenderDevice> {
 
     pub single_gpu: SingleGpuShadows<D>,
 
+    pub keys: HashMap<PhysicalKey, bool>,
+
     pub frames_in_flight: usize,
     pub frame_idx: usize,
     pub camera: Camera,
 
-    pub globals: GpuGlobals,
     pub buffer: Handle<Buffer>,
     pub global_argument: Handle<ShaderArgument>,
+}
+
+fn main() {
+    unsafe {
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
+    let console_log = tracing_subscriber::fmt::Layer::new()
+        .with_ansi(true)
+        .with_writer(std::io::stdout);
+    let subscriber = tracing_subscriber::registry().with(console_log);
+    let _ = tracing::subscriber::set_global_default(subscriber);
+
+    let event_loop = winit::event_loop::EventLoop::new().expect("failed to create event loop");
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
+
+    let mut app = Application::new("Fotia", 800, 600);
+    event_loop.run_app(&mut app).expect("failed to run app");
+}
+
+impl Application<DxDevice> {
+    fn new(title: impl ToString, width: u32, height: u32) -> Application<DxDevice> {
+        let rs = Arc::new(RenderSystem::new(&[RenderBackendSettings {
+            api: RenderBackend::Dx12,
+            debug: if cfg!(debug_assertions) {
+                DebugFlags::all()
+            } else {
+                DebugFlags::empty()
+            },
+        }]));
+
+        let backend = rs.dx_backend().expect("failed to get directx backend");
+
+        let shaders = ShaderCollection::new(&backend, cfg!(debug_assertions));
+
+        let primary = backend.create_device(0);
+        let secondary = backend.create_device(1);
+
+        let group = Arc::new(ContextDual::new(primary, secondary));
+
+        let psos = PsoCollection::new(Arc::clone(&rs), Arc::clone(&group), &shaders);
+
+        let single_gpu = SingleGpuShadows::new(
+            Arc::clone(&rs),
+            Arc::clone(&group.primary),
+            [width, height],
+            &psos,
+            3,
+        );
+
+        let world = World::new();
+
+        let camera = CameraRig::builder()
+            .with(Position::new(glam::Vec3::Y))
+            .with(YawPitch::new())
+            .with(Smooth::new_position_rotation(1.0, 1.0))
+            .build();
+
+        let camera = Camera {
+            far: 1000.0,
+            near: 0.1,
+            fov: 90.0f32.to_radians(),
+            aspect_ratio: width as f32 / height as f32,
+            rig: camera,
+        };
+
+        let buffer = rs.create_buffer_handle();
+        let global_argument = rs.create_shader_argument_handle();
+
+        group.call(|ctx| {
+            ctx.bind_buffer(
+                buffer,
+                rhi::resources::BufferDesc::cpu_to_gpu(
+                    size_of::<GpuGlobals>() * 3,
+                    BufferUsages::Uniform,
+                )
+                .with_name("Global data".into()),
+                None,
+            );
+
+            ctx.bind_shader_argument(
+                global_argument,
+                ra::shader::ShaderArgumentDesc {
+                    views: &[],
+                    samplers: &[],
+                    dynamic_buffer: Some(buffer),
+                },
+            );
+        });
+
+        Application {
+            title: title.to_string(),
+            width,
+            height,
+
+            timer: GameTimer::default(),
+
+            rs,
+            context: group,
+            wnd_ctx: None,
+
+            shaders,
+            psos,
+            single_gpu,
+
+            world,
+            frames_in_flight: 3,
+            frame_idx: 0,
+            camera,
+
+            buffer,
+            global_argument,
+
+            keys: HashMap::new(),
+        }
+    }
+}
+
+impl<D: RenderDevice> Application<D> {
+    fn update(&mut self) {
+        self.camera.update(&self.keys, self.timer.delta_time());
+
+        let view = self.camera.view();
+        let proj = self.camera.proj();
+
+        self.context.call(|ctx| {
+            ctx.update_buffer(
+                self.buffer,
+                self.frame_idx,
+                &[GpuGlobals {
+                    view,
+                    proj,
+                    proj_view: proj * view,
+                    inv_view: view.inverse(),
+                    inv_proj: proj.inverse(),
+                    inv_proj_view: (proj * view).inverse(),
+                    eye_pos: self.camera.rig.final_transform.position.into(),
+                    _pad0: 0.0,
+                    screen_dim: vec2(self.width as f32, self.height as f32),
+                    _pad1: Default::default(),
+                }],
+            );
+        });
+
+        self.single_gpu.update(
+            &self.camera,
+            glam::Vec3::new(-1.0, -1.0, -1.0),
+            self.frame_idx,
+        );
+    }
+
+    fn render(&mut self) {
+        let Some(wnd) = &mut self.wnd_ctx else {
+            return;
+        };
+
+        let frame = wnd.swapchain.next_frame();
+        self.context.call_primary(|ctx| {
+            ctx.wait_on_cpu(CommandType::Graphics, frame.last_access);
+            let mut encoder = ctx.create_encoder(CommandType::Graphics);
+            let timings = encoder.begin(ctx);
+            info!("Timings: {:?}", timings);
+
+            encoder.set_barriers(&[Barrier::Texture(frame.texture, ResourceState::RenderTarget)]);
+            ctx.enqueue(encoder);
+
+            let time = std::time::Instant::now();
+            self.single_gpu.render(
+                &self.world,
+                self.global_argument,
+                frame.texture,
+                self.frame_idx,
+            );
+            info!("CPU TIME: {:?}", time.elapsed());
+
+            let mut encoder = ctx.create_encoder(CommandType::Graphics);
+            encoder.set_barriers(&[Barrier::Texture(frame.texture, ResourceState::Present)]);
+
+            ctx.commit(encoder);
+            frame.last_access = ctx.submit(CommandType::Graphics);
+        });
+
+        wnd.swapchain.present();
+
+        self.frame_idx = (self.frame_idx + 1) % self.frames_in_flight;
+    }
+
+    fn calculate_frame_stats(&self) {
+        thread_local! {
+            static FRAME_COUNT: RefCell<i32> = Default::default();
+            static TIME_ELAPSED: RefCell<f32> = Default::default();
+        }
+
+        FRAME_COUNT.with_borrow_mut(|frame_cnt| {
+            *frame_cnt += 1;
+        });
+
+        TIME_ELAPSED.with_borrow_mut(|time_elapsed| {
+            if self.timer.total_time() - *time_elapsed > 1.0 {
+                FRAME_COUNT.with_borrow_mut(|frame_count| {
+                    let fps = *frame_count as f32;
+                    let mspf = 1000.0 / fps;
+
+                    if let Some(ref context) = self.wnd_ctx {
+                        context
+                            .window
+                            .set_title(&format!("{} Fps: {fps} Ms: {mspf}", self.title))
+                    }
+
+                    *frame_count = 0;
+                    *time_elapsed += 1.0;
+                });
+            }
+        })
+    }
 }
 
 impl<D: RenderDevice> winit::application::ApplicationHandler for Application<D> {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
         let window_attributes = winit::window::Window::default_attributes()
-            .with_title("Hello, Window!")
+            .with_title(&self.title)
             .with_inner_size(winit::dpi::PhysicalSize::new(800, 600));
 
         let window = event_loop.create_window(window_attributes).unwrap();
@@ -184,10 +307,10 @@ impl<D: RenderDevice> winit::application::ApplicationHandler for Application<D> 
 
         let swapchain = self.context.primary.create_swapchain(
             SwapchainDesc {
-                width: 800,
-                height: 600,
+                width: self.width,
+                height: self.height,
                 present_mode: PresentMode::Immediate,
-                frames: 3,
+                frames: self.frames_in_flight,
             },
             &wnd,
             &self.rs.handles,
@@ -219,8 +342,12 @@ impl<D: RenderDevice> winit::application::ApplicationHandler for Application<D> 
                     if event.physical_key == winit::keyboard::KeyCode::Escape {
                         event_loop.exit();
                     }
+
+                    self.keys.insert(event.physical_key, true);
                 }
-                winit::event::ElementState::Released => {}
+                winit::event::ElementState::Released => {
+                    self.keys.insert(event.physical_key, false);
+                }
             },
             winit::event::WindowEvent::MouseInput { state, .. } => match state {
                 winit::event::ElementState::Pressed => {}
@@ -235,55 +362,19 @@ impl<D: RenderDevice> winit::application::ApplicationHandler for Application<D> 
                         &self.rs.handles,
                     );
                     self.single_gpu.resize([size.width, size.height]);
+
+                    self.width = size.width;
+                    self.height = size.height;
                 }
+
+                self.camera.resize([size.width, size.height]);
             }
             winit::event::WindowEvent::RedrawRequested => {
                 self.timer.tick();
                 self.calculate_frame_stats();
 
-                self.single_gpu.update(
-                    &self.camera,
-                    glam::Vec3::new(0.0, -1.0, 0.5),
-                    self.frame_idx,
-                );
-
-                let Some(wnd) = &mut self.wnd_ctx else {
-                    return;
-                };
-
-                let frame = wnd.swapchain.next_frame();
-                self.context.call_primary(|ctx| {
-                    ctx.wait_on_cpu(CommandType::Graphics, frame.last_access);
-                    let mut encoder = ctx.create_encoder(CommandType::Graphics);
-                    let timings = encoder.begin(ctx);
-                    info!("Timings: {:?}", timings);
-
-                    encoder.set_barriers(&[Barrier::Texture(
-                        frame.texture,
-                        ResourceState::RenderTarget,
-                    )]);
-                    ctx.enqueue(encoder);
-
-                    let time = std::time::Instant::now();
-                    self.single_gpu.render(
-                        &self.world,
-                        self.global_argument,
-                        frame.texture,
-                        self.frame_idx,
-                    );
-                    info!("CPU TIME: {:?}", time.elapsed());
-
-                    let mut encoder = ctx.create_encoder(CommandType::Graphics);
-                    encoder
-                        .set_barriers(&[Barrier::Texture(frame.texture, ResourceState::Present)]);
-
-                    ctx.commit(encoder);
-                    frame.last_access = ctx.submit(CommandType::Graphics);
-                });
-
-                wnd.swapchain.present();
-
-                self.frame_idx = (self.frame_idx + 1) % self.frames_in_flight;
+                self.update();
+                self.render();
             }
             winit::event::WindowEvent::CloseRequested => event_loop.exit(),
             _ => (),
@@ -298,7 +389,9 @@ impl<D: RenderDevice> winit::application::ApplicationHandler for Application<D> 
         event: winit::event::DeviceEvent,
     ) {
         match event {
-            winit::event::DeviceEvent::MouseMotion { .. } => {}
+            winit::event::DeviceEvent::MouseMotion { delta } => {
+                self.camera.rotate(delta.0 as f32, delta.1 as f32);
+            }
             _ => {}
         }
     }
@@ -310,33 +403,8 @@ impl<D: RenderDevice> winit::application::ApplicationHandler for Application<D> 
     }
 }
 
-impl<D: RenderDevice> Application<D> {
-    fn calculate_frame_stats(&self) {
-        thread_local! {
-            static FRAME_COUNT: RefCell<i32> = Default::default();
-            static TIME_ELAPSED: RefCell<f32> = Default::default();
-        }
-
-        FRAME_COUNT.with_borrow_mut(|frame_cnt| {
-            *frame_cnt += 1;
-        });
-
-        TIME_ELAPSED.with_borrow_mut(|time_elapsed| {
-            if self.timer.total_time() - *time_elapsed > 1.0 {
-                FRAME_COUNT.with_borrow_mut(|frame_count| {
-                    let fps = *frame_count as f32;
-                    let mspf = 1000.0 / fps;
-
-                    if let Some(ref context) = self.wnd_ctx {
-                        context
-                            .window
-                            .set_title(&format!("{} Fps: {fps} Ms: {mspf}", "Fotia"))
-                    }
-
-                    *frame_count = 0;
-                    *time_elapsed += 1.0;
-                });
-            }
-        })
+impl<D: RenderDevice> Drop for Application<D> {
+    fn drop(&mut self) {
+        self.context.call(|ctx| ctx.wait_idle());
     }
 }
